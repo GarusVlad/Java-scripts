e8e8a37140984f7339f3d94010704b9ccf4ccab2
GarusVlad <90756147+GarusVlad@users.noreply.github.com>
2023-04-02 18:01:37 +0300

    Работа с переменными

---
+++ 2.4.js
@@ -0,0 +1,17 @@
+// 1. 
+let name;
+let admin:
+
+name = "Джон";
+
+admin = name;
+
+alert(name);
+
+// 2.
+let MyPlanet = "Земля";
+let UserName = "Константин";
+
+// 3.
+const Birthday = "21.07.2004";
+const age = someCode(Birthday);
